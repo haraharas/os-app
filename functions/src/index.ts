42aa55eec290eba2de2756937d43e95abef801cb
haraharas <s.uehara@offices.jp>
2023-10-10 17:00:38 +0900

    lint修正

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -7,6 +7,11 @@
  * See a full list of supported triggers at https://firebase.google.com/docs/functions
  */
 
+import {onRequest} from "firebase-functions/v2/https";
+import * as logger from "firebase-functions/logger";
+console.log(onRequest);
+console.log(logger);
+
 // Start writing functions
 // https://firebase.google.com/docs/functions/typescript
 
